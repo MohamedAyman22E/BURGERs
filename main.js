6a41891957f2c87ee3ea52e4d91bb14a1fc21731
moAymanEz <moAyman@gmail.com>
2023-11-03 23:12:15 +0200

    End of project

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let menu = document.getElementById("icon_menu");
 let navList = document.querySelector(".navList");
 let menuI = document.querySelector(".icon_menu I");
+let navListLink = document.querySelectorAll(".navList ");
 menu.addEventListener("click", () => {
   navList.classList.toggle("hidden");
   if (menuI.classList.contains("fa-bars")) {
@@ -8,4 +9,60 @@ menu.addEventListener("click", () => {
   } else {
     menuI.classList.replace("fa-xmark", "fa-bars");
   }
+});
+
+navListLink.forEach(li => {
+  li.addEventListener("click", () => {
+    navList.classList.add("hidden");
+  });
+});
+
+let taps = document.querySelectorAll(".tabs_wrap ul li");
+let all = document.querySelectorAll(".ulFilter li");
+taps.forEach(tap => {
+  tap.addEventListener("click", () => {
+    taps.forEach(tapRemove => {
+      tapRemove.classList.remove("active");
+    });
+    tap.classList.add("active");
+    let tapsVal = tap.getAttribute("data-taps");
+    all.forEach(item => {
+      if (tapsVal == item.getAttribute("data-category")) {
+        item.classList.add("show");
+        item.classList.remove("hide");
+      } else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+      if (tapsVal === "all") {
+        item.classList.add("show");
+        item.classList.remove("hide");
+      }
+    });
+  });
+});
+
+var swiper = new Swiper(".mySwiper", {
+  speed: 400,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
     screens: {
       280: "280px",
       350: "350px",
+      500: "500px",
       810: "810px",
       1000: "1000px",
+      1100: "1100px",
       1200: "1200px",
+      1250: "1250px",
       1445: "1445px",
     },
     extend: {
@@ -22,6 +25,19 @@ module.exports = {
         redColor: "#cc3433",
         darkColor: "#000",
         darkColorLight: "#171717",
+      },
+      keyframes: {
+        move: {
+          "50%": { transform: "translateY(-1rem)" },
+        },
+        moveBox: {
+          "0%": { opacity: "0", transform: "scale(0.9)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
+        },
+      },
+      animation: {
+        movingY: "move 2s linear infinite",
+        moveBoxAnim: "moveBox .5s linear ",
       },
       container: {
         center: true,
